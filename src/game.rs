fecafa6afc8c9660f9d7be8792541189dc1020aa
Eugene Lomov <eugene.v.lomov@gmail.com>
2018-01-11 02:25:59 +0300

    Right ready

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,15 @@
-type Max_num = u16;
+pub type Max_num = u16;
 pub struct Game {
-    Board: Vec<Vec<Max_num>>,
-    Score: u64,
+    pub Board: Vec<Vec<Max_num>>,
+    pub Score: u64,
 }
 
 impl Game {
+    pub fn print(&self) {
+        for row in &self.Board {
+            println!("{:?}", row);
+        }
+    }
     pub fn new() -> Game {
         Game {
             Board : vec![vec![0 as Max_num; 4]; 4],
@@ -20,32 +25,34 @@ impl Game {
         }
         return score;
     }
-    // pub fn Right(&mut self) -> u64 {
-    //     let mut score: u64 = 0;
-    //     for mut row in &mut self.Board {
-    //         let mut temp: Vec<Max_num> = Vec::new();
-    //         for elem in row {
-    //             temp.insert(0, *elem);
-    //         }
-    //         score += Game::Shift(&mut temp);
-    //         for i in 0..row.len() {
-    //             row[i] = temp[i];
-    //         }
-    //     }
-    //     return score;
+    pub fn Right(&mut self) -> u64 {
+        let mut score: u64 = 0;
+        for mut row in &mut self.Board {
+            let mut temp: Vec<Max_num> = Vec::new();
+            for elem in row.iter() {
+                temp.insert(0, *elem);
+            }
+            score += Game::Shift(&mut temp);
+            row.clear();
+            for i in 0..temp.len() {
+                // row[i] = temp[temp.len() - 1 - i];
+                row.insert(0, temp[i]);
+            }
+        }
+        return score;
     }
     fn Shift(row: &mut Vec<Max_num>) -> u64{
         let mut score: u64 = 0;
         for i in 0..row.len() - 1 {
-            println!("{:?}", row);
+            // println!("{:?}", row);
             if (row[i] == 0) & (row[i+1] != 0) {
                 row.remove(i);
                 row.push(0);
             }
         }
-        println!("");
+        // println!("");
         for i in 0..row.len() - 1 {
-            println!("{:?}", row);
+            // println!("{:?}", row);
             if (row[i] == row[i+1]) & (row[i] != 0) {
                 row[i] *= 2;
                 score += row[i] as u64;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod game;
 fn main() {
-    let mut my_game = game::Game::new();
+    // let mut my_game = game::Game::new();
+    let mut my_game = game::Game {
+        Board : vec![vec![4 as game::Max_num, 4 as game::Max_num, 2 as game::Max_num, 4 as game::Max_num]; 4],
+        Score : 0,
+    };
+    my_game.print();
+    println!("");
+    // my_game.Left();
+    my_game.Right();
+    my_game.print();
     // let mut test:Vec<u16> = vec![0,2,2,2,2,4,4,8,2,4,8];
     // println!("{:?}", test);
     // game::Game::Shift(&mut test);
