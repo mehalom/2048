cbc731b73b4ec53028706daafe2e82a961212241
Eugene Lomov <eugene.v.lomov@gmail.com>
2018-01-11 02:06:45 +0300

    Left ready

--- src/game.rs
+++ src/game.rs
@@ -13,34 +13,46 @@ impl Game {
     }
     // pub fn Up(&mut self) -> Game;
     // pub fn Down(&mut self) -> Game;
-    pub fn Left(&mut self) {
+    pub fn Left(&mut self) -> u64{
+        let mut score: u64 = 0;
         for mut row in &mut self.Board {
-            Game::Shift(row);
+            score += Game::Shift(row);
         }
+        return score;
     }
-    // pub fn Right(&self) -> Game;
-    fn Shift(row: &mut Vec<Max_num>){
-        let mut flag: bool = true;
-        while flag == true {
-            flag = false;
-            for i in 0..row.len() - 1 {
-                // println!("{:?}", row);
-                // println!("{} {} {}", i, row[i], row[i+1]);
-                if (row[i] == row[i+1]) & (row[i] != 0) {
-                    row[i] *= 2;
-                    row.remove(i+1);
-                    row.push(0);
-                    flag = true;
-                    break;
-                } else if (row[i] == 0) & (row[i+1] != 0) {
-                    row.remove(i);
-                    row.push(0);
-                    flag = true;
-                    break;
-                }
+    // pub fn Right(&mut self) -> u64 {
+    //     let mut score: u64 = 0;
+    //     for mut row in &mut self.Board {
+    //         let mut temp: Vec<Max_num> = Vec::new();
+    //         for elem in row {
+    //             temp.insert(0, *elem);
+    //         }
+    //         score += Game::Shift(&mut temp);
+    //         for i in 0..row.len() {
+    //             row[i] = temp[i];
+    //         }
+    //     }
+    //     return score;
+    }
+    fn Shift(row: &mut Vec<Max_num>) -> u64{
+        let mut score: u64 = 0;
+        for i in 0..row.len() - 1 {
+            println!("{:?}", row);
+            if (row[i] == 0) & (row[i+1] != 0) {
+                row.remove(i);
+                row.push(0);
             }
         }
+        println!("");
+        for i in 0..row.len() - 1 {
+            println!("{:?}", row);
+            if (row[i] == row[i+1]) & (row[i] != 0) {
+                row[i] *= 2;
+                score += row[i] as u64;
+                row.remove(i+1);
+                row.push(0);
+            }
+        }
+        return score;
     }
-
-
 }
