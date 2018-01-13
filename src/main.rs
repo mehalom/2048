37f31de44b4372de4c5b5f40b1eb33e22afce588
Eugene Lomov <eugene.v.lomov@gmail.com>
2018-01-13 13:28:53 +0300

    Added clean()

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,27 @@ extern crate rand;
 extern crate console;
 use console::Term;
 fn main() {
-    println!("2042 in Rust v0.0.1");
+    println!("2048 in Rust v0.0.1");
     let mut my_game = game::Game::new();
     let term = Term::stdout();
     my_game.add();
     my_game.add();
-    term.clear_last_lines(60);
+    clean(&term);
     my_game.print();
     loop {
         while ! my_game.inp() {
-            term.clear_last_lines(60);
+            term.clear_last_lines(7);
             my_game.print();
         }
         my_game.add();
-        term.clear_last_lines(60);
+        term.clear_last_lines(6);
         my_game.print();
 
     }
+}
+fn clean(term: &Term) {
+    for _ in 0..20 {
+        println!("");
+    }
+    term.clear_last_lines(20);
 }
